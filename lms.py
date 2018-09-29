1305567a5a290d2f2873519cc3c4ec0ab0074970
fsousa <fesousa>
2018-09-29 17:34:39 -0300

    Atualizacao api

--- lms.py
+++ lms.py
@@ -1,5 +1,9 @@
 
 from flask import Flask, jsonify, request
+import json
+import urllib.request
+import random
+
 app = Flask(__name__)
 
 disciplinas = [{"id": e, "nome": "Disciplina "+str(e), "ementa":"Ementa "+str(e), "foto":"https://cdn.pixabay.com/photo/2018/01/18/20/42/pencil-3091204_1280.jpg", "professor": "Professor Disciplina "+str(e)} for e in range(1,11)]   
@@ -8,13 +12,13 @@ disciplinas = [{"id": e, "nome": "Disciplina "+str(e), "ementa":"Ementa "+str(e)
 def get():
     return jsonify(disciplinas)
 
-# @app.route("/disciplinas/<int:id>/")
-# def get(id):
-#     filtro = [e for e in disciplinas if e["id"] == id]
-#     if filtro:
-#         return jsonify(filtro[0])
-#     else:
-#         return jsonify({})
+@app.route("/disciplinas/<int:id>", methods=['GET'])
+def get_one(id):
+    filtro = [e for e in disciplinas if e["id"] == id]
+    if filtro:
+        return jsonify(filtro[0])
+    else:
+        return jsonify({})
 
 @app.route("/disciplinas", methods=['POST'])
 def post():
@@ -34,7 +38,7 @@ def post():
     except Exception as ex:
         return jsonify({"status":"ERRO", "msg":str(ex)})
 
-@app.route("/disciplinas/<int:id>/", methods=['DELETE'])
+@app.route("/disciplinas/<int:id>", methods=['DELETE'])
 def delete(id):
     global disciplinas
     try:
@@ -42,6 +46,29 @@ def delete(id):
         return jsonify({"status":"OK", "msg":"disciplina removida com sucesso"})
     except Exception as ex:
         return jsonify({"status":"ERRO", "msg":str(ex)})
+
+@app.route("/push/<string:key>/<string:token>", methods=['GET'])
+def push(key, token):
+	d = random.choice(disciplinas)
+	data = {
+		"to": token,
+		"notification" : {
+			"title":d["nome"],
+			"body":"Você tem nova atividade em "+d['nome']
+		},
+		"data" : {
+			"disciplinaId":d['id']
+		}
+	}
+	req = urllib.request.Request('http://fcm.googleapis.com/fcm/send')
+	req.add_header('Content-Type', 'application/json')
+	req.add_header('Authorization', 'key='+key)
+	jsondata = json.dumps(data)
+	jsondataasbytes = jsondata.encode('utf-8')   # needs to be bytes
+	req.add_header('Content-Length', len(jsondataasbytes))
+	response = urllib.request.urlopen(req, jsondataasbytes)
+	print(response)
+	return jsonify({"status":"OK", "msg":"Push enviado"})
 
 
 if __name__ == "__main__":
